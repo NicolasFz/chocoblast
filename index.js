ad3f1a1e41ffc58fde8d2f384ed6e3fecb66f71a
NicolasFz <nfernandez@Nicolas-PC.localdomain>
2020-07-12 16:25:10 +0200

    Allow players to reconnect

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
+SOCKET_LIST = {};
+var players = {};
+var chocoblasted = null;
+
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/chocoblast/index.html');
 });
@@ -13,20 +17,10 @@ app.get('/helloworld', function(req, res) {
 
 app.use('/chocoblast', express.static(__dirname + '/chocoblast'));
 
-
 console.log("Server started.");
 
-SOCKET_LIST = {};
-var players = {};
-var chocoblasted = null;
-
 io.sockets.on('connection', function(socket) {
-
-    console.log('new user!');
-    var playerId = Math.random();
-    SOCKET_LIST[playerId] = socket;
-    players[playerId] = { id: playerId, pseudo: 'anon', score: 0, move: null }
-    socket.emit('setPlayerId', playerId);
+    var playerId = handlePlayerConnection(socket);
 
     // Refresh the list of player for each connected players
     refreshPlayerList();
@@ -40,22 +34,54 @@ io.sockets.on('connection', function(socket) {
 
     socket.on('disconnect', function() {
         delete SOCKET_LIST[playerId];
-        delete players[playerId];
+        // delete players[playerId];
     });
 
 });
+
+
+function handlePlayerConnection(socket) {
+    if (socket.request._query['player-id'] != null) {
+        var playerId = socket.request._query['player-id'];
+        if (players[playerId] != undefined) {
+            reconnectPlayer(socket, players[playerId]);
+            return playerId;
+        } else {
+            socket.disconnect('unauthorized');
+        }
+    } else {
+        return createANewPlayer(socket);
+    }
+}
+
+function reconnectPlayer(socket, player) {
+    console.log("Player " + player.pseudo + " is reconnected");
+    SOCKET_LIST[player.id] = socket;
+    socket.emit('setPlayerId', player.id);
+    socket.emit('pseudoSaved', player.pseudo);
+    socket.emit('playerConnected');
+}
+
+function createANewPlayer(socket) {
+    console.log("New player created");
+    var playerId = Math.random();
+    SOCKET_LIST[playerId] = socket;
+    players[playerId] = { id: playerId, pseudo: 'anon', score: 0, move: null }
+    socket.emit('setPlayerId', playerId);
+    socket.emit('playerConnected');
+    return playerId;
+}
 
 function changeChocoblasted() {
     let playerIdList = Object.keys(players);
     if (playerIdList.length > 1) {
         let chocoblastedId = playerIdList[Math.floor(Math.random() * playerIdList.length)];
-        let chocoblasted = players[chocoblastedId]
+        chocoblasted = players[chocoblastedId]
         console.log('The new chocoblasted is :' + chocoblasted.pseudo);
         broadcastToPlayers('changeChocoblasted', chocoblasted)
     } else {
         console.log('Not enough player to start the chocoblasting');
     }
-
 }
 
 function refreshPlayerList() {
